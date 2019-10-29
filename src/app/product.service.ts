6111c0b98a1f34c8738ff45826578a07692aa98d
NgonidzNashe Utete <ngonie@live.com>
2019-10-29 12:24:07 +0200

    Mapping The JSON Response in getAlbum

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { Observable } from 'rxjs';
+import { map} from 'rxjs/operators';
 
 @Injectable()
 export class ProductService {
@@ -9,7 +11,8 @@ export class ProductService {
   constructor(private _http: Http) { }
   
   getAlbum(id:number){
-    return this._http.get(this._albumUrl);
+    return this._http.get(this._albumUrl)
+    .map(response.json());
   }
 
 }
