50909ed26cc295c07f7ad0efa399dce93f6b6c2d
NgonidzNashe Utete <ngonie@live.com>
2019-10-29 12:36:28 +0200

    Inject The Service Into Product Description’s Constructor

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(_productService:ProductService) { }
 
   ngOnInit() {
   }
