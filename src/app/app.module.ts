d822ca88451e796bf2ecda5c266841c6d6b35f31
henry uyigue <don2much@gmail.com>
2020-10-02 12:25:49 -0700

    update error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 
 
+
+
 const appRoutes: Routes = [
   {path: 'products', component: ProductListComponent},
   {path: 'product/:id', component: ProductPageComponent},
